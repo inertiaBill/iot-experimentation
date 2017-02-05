25bdfebd625ec3d190e01739317605124fc97300
inertiaBill <inertiaBill@users.noreply.github.com>
2017-02-04 19:56:26 -0500

    Add support for custom character output to LED
    Started to build up the Gyro Clear game.
    - Added a GyroClear class to house the game.
    - Moved environment monitoring into the class, but may want to move
    to a separate class later.
    - Added support for custom characters to be displayed on LED display.
    - Added a check and output of temperature from pressure sensor. The
    original check is temperature from the humitity sensor.

--- sense-hat/sensefun.py
+++ sense-hat/sensefun.py
@@ -1,20 +1,83 @@
+""" Sense hat screen clearing game.
+"""
+
 import time
 from sense_hat import SenseHat
 
-sense = SenseHat()
+class GyroClear(object):
+    #Initialize class variables here
+    b = (0, 0, 255) # blue
+    c = (0, 0, 0) # clear
 
-temp = sense.get_temperature()
-sense.show_message("T:")
-time.sleep(1)
-sense.show_message(str(int(temp)))
-time.sleep(1)
-pressure = sense.get_pressure()
-sense.show_message("P:")
-time.sleep(1)
-sense.show_message(str(int(pressure)))
-time.sleep(1)
-humidity = sense.get_humidity()
-sense.show_message("H:")
-time.sleep(1)
-sense.show_message(str(int(humidity)))
-time.sleep(1)
+    def __init__(self):
+        super(GyroClear, self).__init__()
+        self.__sense = SenseHat()
+        self.__a = [
+                GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c,
+                GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b,
+                GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b,
+                ]
+        self.__s = [
+                GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c,
+                GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.c, GyroClear.c,
+                ]
+        self.__p = [
+                GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.b, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.b, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c,
+                GyroClear.c, GyroClear.b, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c, GyroClear.c,
+                ]
+
+    def show_letter(self, letter):
+        self.__sense.show_letter(letter)
+        time.sleep(2)
+
+    def clear_display(self):
+        self.__sense.clear()
+
+    def display_letters(self):
+        time.sleep(1)
+        self.__sense.set_pixels(self.__a)
+        time.sleep(2)
+        self.__sense.set_pixels(self.__s)
+        time.sleep(2)
+        self.__sense.set_pixels(self.__p)
+        time.sleep(2)
+
+    def display_current_readings(self):
+        temp = self.__sense.get_temperature()
+        self.__sense.show_message("Th: ")
+        self.__sense.show_message(str(int(temp)))
+        temp = self.__sense.get_temperature_from_pressure()
+        self.__sense.show_message("Tp: ")
+        self.__sense.show_message(str(int(temp)))
+        pressure = self.__sense.get_pressure()
+        self.__sense.show_message("P: ")
+        self.__sense.show_message(str(int(pressure)))
+        humidity = self.__sense.get_humidity()
+        self.__sense.show_message("H: ")
+        self.__sense.show_message(str(int(humidity)))
+
+if __name__ == "__main__":
+    game = GyroClear()
+    game.display_current_readings()
+    game.display_letters()
+    game.show_letter('a')
+    game.show_letter('s')
+    game.show_letter('p')
+    game.clear_display()
